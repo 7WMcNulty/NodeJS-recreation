23341a5b7560bc1d67180d9aba488b9b698becc8
7WMcNulty <7WMcNulty@stpatrick.org>
2017-04-20 13:57:57 -0500

    Just about done

--- hover.js
+++ hover.js
@@ -1,5 +1,24 @@
 (function(){
   const doc=document;
   let pic=doc.querySelector("#pic");
-  
+  pic.addEventListener("mouseover",
+  function(){
+    pic.style.borderColor = "green";
+  }
+);
+  pic.addEventListener("mouseout",
+  function(){
+    pic.style.borderColor = "white";
+  }
+);
+  let divs = doc.querySelectorAll(".notbutton");
+  for(let i=0; i<divs.length; i++){
+    divs[i].addEventListener("mouseover", function(){
+      divs[i].style.opacity = 0.5;
+    });
+    divs[i].addEventListener("mouseout", function(){
+      divs[i].style.opacity = 1;
+    });
+  };
+
 })();
